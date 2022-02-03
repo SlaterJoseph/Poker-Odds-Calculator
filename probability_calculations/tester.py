ae4437c656b85586017b4ed50ce383e655212e5a
Joseph Slater <josephslater03@gmail.com>
2022-02-02 22:46:27 -0500

    completed 2 pair probability, cleaned pair probability

--- probability_calculations/tester.py
+++ probability_calculations/tester.py
@@ -2,13 +2,25 @@ import sys
 sys.path.append('../poker_components')
 from card import Card
 from pair_probability import calculate_pair
+from two_pair_probability import calculate_two_pair
 #testing pair
+# hand = [Card("heart", 1), Card("heart", 2)]
+# table = []
+# print(calculate_pair(hand, table, "preflop"))
+# table = [Card("heart", 1), Card("heart", 4), Card("heart", 5)]
+# print(calculate_pair(hand, table, "flop"))
+# table.append(Card("heart", 5))
+# print(calculate_pair(hand, table, "turn"))
+# table.append(Card("heart", 6))
+# print(calculate_pair(hand, table, "river"))
+
+#testing 2 pairs
 hand = [Card("heart", 1), Card("heart", 2)]
 table = []
-print(calculate_pair(hand, table, "preflop"))
-table = [Card("heart", 1), Card("heart", 4), Card("heart", 5)]
-print(calculate_pair(hand, table, "flop"))
-table.append(Card("heart", 5))
-print(calculate_pair(hand, table, "turn"))
+print(calculate_two_pair(hand, table, "preflop"))
+table = [Card("heart", 1), Card("heart", 3), Card("heart", 5)]
+print(calculate_two_pair(hand, table, "flop"))
 table.append(Card("heart", 6))
-print(calculate_pair(hand, table, "river"))
+print(calculate_two_pair(hand, table, "turn"))
+table.append(Card("heart", 7))
+print(calculate_two_pair(hand, table, "river"))
--- probability_calculations/two_pair_probability.py
+++ probability_calculations/two_pair_probability.py
@@ -34,36 +34,112 @@ def calculate_two_pair(hand, table, round):
 
         else:
             probability = [flop(hand), turn(hand, table[0:3]), river(hand, table[0:4]), 0]
-
     return probability
     
 def flop(hand):
     ranks = {hand[0].get_value(), hand[1].get_value()}
+    if len(ranks) == 1:
+        flop_two_pair = ((66 * 4 * 6) / 19600) + ((12 * 4) / 19600) + ((2 * 12 * 6) / 19600)
+
+    else:
+        flop_two_pair = ((3 * 3 * 11 * 4) / 19600) + ((2 * 3 * 11 * 6) / 19600) + ((2 * 3 * 3) / 19600)
     return truncate(flop_two_pair)
 
 def turn(hand, table = 'n/a'):
     if table == 'n/a': #No flop is available yet
-        
+        ranks = {hand[0].get_value(), hand[1].get_value()}
+        if len(ranks) == 1:
+           turn_two_pair = ((220 * 4 * 4 * 4)/19600) * (9 / 47) + ((2 * 66 * 4 * 4)/19600) * (6 / 47) \
+           + ((12 * 4)/19600) * (3 / 47)
+
+        else:
+            turn_two_pair =  (((2 * 3 * 55 * 4 * 4) / 19600) * (9 / 47)) + (((55 * 6 * 4) / 19600) * (9 / 47)) \
+            + (((11 * 4) / 19600) * ((6 / 47))) + ((2 / 19600) * (3 / 47)) + (((2 * 3 * 11 * 4) / 19600) * (6 / 47))
     else:
-        
+        ranks = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0}
+        ranks[hand[0].get_value()] += 1
+        ranks[hand[1].get_value()] += 1
+        ranks[table[0].get_value()] += 1
+        ranks[table[1].get_value()] += 1
+        ranks[table[2].get_value()] += 1
+
+        if process_matches(ranks, 4) == 1: # if we have fours, only 1 card can make a pair on the flop
+            turn_two_pair = 3 / 47 
+
+        elif process_matches(ranks, 3) == 1: #if we have 3s, 2 cards can make a pair on the flop
+            turn_two_pair = 6 / 47 
+
+        elif process_matches(ranks, 2) == 1: #if we have 1 pair, 3 cards can make a pair on the flop
+            turn_two_pair = 9 / 47 
+
+        else: #either we have no pairs or 2 pairs
+            turn_two_pair = 0 if process_matches(ranks, 2) == 0 else 100
     return truncate(turn_two_pair)
 
 def river(hand, table = 'n/a'):
     if table == 'n/a':
+        ranks = {hand[0].get_value(), hand[1].get_value()}
+        if len(ranks) == 1:
+            river_two_pair = (((2 * 66 * 4 * 4) / 19600) * ((10 * 4) / 47) * (9 / 46)) \
+            + (((220 * 4 * 4 * 4) / 19600) * ((9 * 4) / 47) * (12 / 46)) + (((12 * 4) / 19600) * ((11 * 4) / 47) * (6 / 46)) \
+            + (((220 * 4 * 4 * 4) / 19600) * ((2 * 4) / 47) * (9 / 46)) + (((2 * 66 * 4 * 4) / 19600) * (1 / 47) * (9 / 46))
+        else:
+            river_two_pair =  (((2 * 3 * 55 * 4 * 4) / 19600) * ((9 * 4) / 47) * (12 / 46)) \
+            + (((2 * 3 * 11 * 4) / 19600) * ((10 * 4) / 47) * (9 / 46)) + ((2  / 19600) * ((11 * 4) / 47) * (6 / 46)) \
+            + (((55 * 6 * 4) / 19600) * ((9 * 4) / 47) * (12 / 46)) + (((11 * 4) / 19600) * ((10 * 4) / 47) * (9 / 46)) \
+            + (((165 * 4 * 4 * 4) / 19600) * ((2 * 3) / 47) * (12 / 46)) \
+            + (((2 * 3 * 55 * 4 * 4) / 19600) * (2 / 47) * (9 / 46)) + (((2 * 3 * 11 * 4 * 4) / 19600) * (1 / 47) * (6 / 46)) \
+            + (((165 * 4 * 4 * 4) / 19600) * ((3 * 3) / 47) * (12 / 46)) \
+            + (((55 * 6 * 4) / 19600) * (2 / 47) * (9 / 46)) + (((11 * 4) / 19600) * (1 / 47) * (6 / 46))
 
     elif len(table) == 3:
-        
+        ranks = dict({1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0})
+        ranks[hand[0].get_value()] += 1
+        ranks[hand[1].get_value()] += 1
+        ranks[table[0].get_value()] += 1
+        ranks[table[1].get_value()] += 1
+        ranks[table[2].get_value()] += 1
+
+        if process_matches(ranks, 4) == 1: # if we have fours, only 1 card can make a pair on the flop
+            river_two_pair = ((11 * 4) / 47) * (6 / 46) 
+
+        elif process_matches(ranks, 3) == 1: #if we have 3s, 2 cards can make a pair on the flop
+            river_two_pair = (2 / 47) * (6 / 46) + ((10 * 4) / 47) * (9 / 46) 
+
+        elif process_matches(ranks, 2) == 1: #if we have 1 pair, 3 cards can make a pair on the flop
+            river_two_pair = ((9 * 4) / 47) * (12 / 46) + (2 / 47) * (9 / 46)  
+
+        else: #either we have no pairs or 2 pairs
+            river_two_pair = ((5 * 3) / 47) * ((4 * 3) / 46) if process_matches(ranks, 2) == 2 else 0
     else:
-        
+        ranks = dict({1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0})
+        ranks[hand[0].get_value()] += 1
+        ranks[hand[1].get_value()] += 1
+        ranks[table[0].get_value()] += 1
+        ranks[table[1].get_value()] += 1
+        ranks[table[2].get_value()] += 1
+        ranks[table[3].get_value()] += 1
+        if process_matches(ranks, 4) == 1: # if we have fours, only 1 card can make a pair on the flop
+            river_two_pair = (6 / 46) 
+
+        elif process_matches(ranks, 3) == 1: #if we have 3s, 2 cards can make a pair on the flop
+            river_two_pair = (9 / 46) 
+
+        elif process_matches(ranks, 2) == 1: #if we have 1 pair, 3 cards can make a pair on the flop
+            river_two_pair = (12 / 46)  
+
+        else: #either we have no pairs or 2 pairs
+            river_two_pair = 0 if process_matches(ranks, 2) == 0 else 100
+
     return truncate(river_two_pair)
 
 def final_check(hand, table = 'n/a'):
     ranks = dict({1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0, 13:0})
-    ranks[hand[0].get_value] += 1
-    ranks[hand[1].get_value] += 1
-    ranks[table[0].get_value] += 1
-    ranks[table[1].get_value] += 1
-    ranks[table[2].get_value] += 1
-    ranks[table[3].get_value] += 1
-    ranks[table[4].get_value] += 1
+    ranks[hand[0].get_value()] += 1
+    ranks[hand[1].get_value()] += 1
+    ranks[table[0].get_value()] += 1
+    ranks[table[1].get_value()] += 1
+    ranks[table[2].get_value()] += 1
+    ranks[table[3].get_value()] += 1
+    ranks[table[4].get_value()] += 1
     return 100 if process_matches(ranks, 2) >= 2 else 0
